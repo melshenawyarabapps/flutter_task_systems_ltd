60c0f0096b0ef1efca24001f52ffb9c7b5130597
melshenawy <114089659+melshenawyarabapps@users.noreply.github.com>
2025-12-12 17:01:48 +0200

    Update app icons and splash screens for all platforms
    Replaced and added new launcher icons and splash screen assets for Android, iOS, macOS, and web. Updated Android and iOS launch screen configurations, manifest, and styles to support new assets and improve appearance. Registered new plugins for macOS. Removed the default widget test.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.flutter_task_sys_ltd"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
